efcdb82c4b5c64fe5bb0c22a88fd2e85fa7cb5d3
Adam Bihari <adam.bihari@gmail.com>
2018-04-19 12:11:36 +0300

    start grafana setup

--- kops/automate.sh
+++ kops/automate.sh
@@ -26,11 +26,16 @@ done
 echo "===Deploying Helm==="
 kubectl create -f helm/helm-rbac-config.yaml
 helm init --service-account tiller
+echo "Waiting 60 seconds for tiller to deploy"
+sleep 60s
 echo "===Deploying ExternalDNS==="
-#kubectl create -f k8s/externaldns-rbac-deployment.yaml
 helm install --name xdns stable/external-dns -f helm/xdns-values.yaml
 echo "===Deploying ingress controller==="
 helm install --name ingcon stable/nginx-ingress -f helm/nginx-values.yaml
+echo "===Deploying Prometheus==="
+helm install --name prometheus stable/prometheus -f helm/prometheus-values.yaml
+echo "===Deploying Grafana==="
+helm install --name grafana stable/grafana -f helm/grafana-values.yaml
 echo "===Deploying spree==="
 kubectl create -f k8s/spree-deployment-ingcon.yaml
 kubectl create -f k8s/spree-ingress.yaml
